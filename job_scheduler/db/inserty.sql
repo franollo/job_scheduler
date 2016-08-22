35a455f8b504cbc91074dcb0571c0f35b54481e1
franollo <marcinfrank1@gmail.com>
2016-08-22 02:17:08 +0200

    Production plan processing

--- job_scheduler/db/inserty.sql
+++ job_scheduler/db/inserty.sql
@@ -9,8 +9,8 @@ values(1, 'rower', 'rower_szosowy', 1);
 insert into resource_types(resource_type_id, name, group_id) values (1, 'tokarki', 1);
 insert into resources (name, description, cost_per_hour, efficiency, resource_type_id, group_id)
 values('Zasób', 'Opis zasobu', 1, 1, 1, 1);
-insert into items (production_plan_id, start, end, resource_id, group_id) 
-values (1, '2016-01-01 00:00:00', '2016-01-01 00:00:10', 1, 1);
+insert into items (production_plan_id, start, end, resource_id, group_id, product_id) 
+values (1, '2016-01-01 00:00:00', '2016-01-01 00:00:10', 1, 1, 1);
 insert into orders(name, description, production_plan_id, group_id) 
 values ('Zamówienie', 'Opis zamówienia', 1, 1);
 insert into order_products values (1, 1, 3, 1);
